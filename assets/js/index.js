581abe395c41e4085e42375d01ad51f545c13b9b
giannurwana19 <giannurwana19@gmail.com>
2023-01-28 19:37:49 +0700

    membuat hamburger menu dan open close sidebar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,19 @@
 const navbar = document.querySelector('.navbar');
+const menuBar = document.querySelector('.ri-menu-line');
+const sidebar = document.querySelector('.navigation');
+const iconClose = document.querySelector('.ri-close-line');
 
 window.addEventListener('scroll', () => {
   const positionWindow = window.scrollY > 0;
 
   navbar.classList.toggle('scrolling-active', positionWindow);
+  sidebar.classList.remove('menu-active');
+});
+
+menuBar.addEventListener('click', () => {
+  sidebar.classList.toggle('menu-active');
+});
+
+iconClose.addEventListener('click', () => {
+  sidebar.classList.remove('menu-active');
 });
